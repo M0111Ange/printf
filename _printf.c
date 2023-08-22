475a575ec0440eba1682598213acdd8adb470ad1
M0111Ange <angemukundente@gmail.com>
2023-08-21 23:38:54 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,47 +1,67 @@
 #include "main.h"
+#define BUFF_SIZE 1024
+void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - prints characters, symbols, strings, etc.
- * @format: char to print
- * Return: integer
+ * _printf - function implements original printf
+ * @format: format specifiers input
+ * by Israel and Dayo
+ * Return: Printed chars str and %
  */
-
 int _printf(const char *format, ...)
 {
-	int i, j, len = 0;
-	va_list args;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
 
-	func arr[] = {
-	{"%d", print_int}, {"%i", print_int},
-	{"%c", print_char}, {"%s", print_string},
-	{"%%", print_sym}
-	};
-
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	if (format == NULL)
 		return (-1);
 
-	va_start(args, format);
+	va_start(list, format);
 
-	for (i = 0; format[i] != '\0';)
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		j = 0;
-		while (j < 5)  /* Number of elements in the arr array */
+		if (format[i] != '%')
 		{
-			if (format[i] == arr[j].s[0] && format[i + 1] == arr[j].s[1])
-			{
-				len += arr[j].ptr(args);
-				i = i + 2;
-				break;
-			}
-			j++;
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
 		}
-		if (j == 5)
+		else
 		{
-			_putchar(format[i]);
-			len++;
-			i++;
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+					       flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
 		}
 	}
-	va_end(args);
-	return (len);
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
